2e7007d74ed7011d486c7fd108de4da7c013e0b7
AUniqueSoulE <aunis87@icloud.com>
2024-10-15 19:48:36 -0400

    testing comment endpoints.

--- routers/commentRoutes.js
+++ routers/commentRoutes.js
@@ -1,31 +1,53 @@
-const express = require('express'); 
+const express = require('express');
 const router = express.Router();
 const Blog = require('../Models/Blog');
 const Comment = require('../Models/Comment');
 
-// POST a comment on a blog
-router.post('/', async (req, res) => {
-    const { content, author } = req.body;
-    try {
-        // Find the blog by ID passed in the URL
-        //const blog = await Blog.findById(req.params.blogId);
-        // if (!blog) {
-        //     return res.status(404).json({ error: 'Blog not found' });
-        // }
+// Dummy comment data
+let comments = [
+  { id: 1, author: 'Author1', content: 'Great blog post!', blogId: 1 },
+  { id: 2, author: 'Author2', content: 'Very informative.', blogId: 1 }
+];
 
-        // Create a new comment
-        const newComment = new Comment({
-            // postId: req.params.blogId,
-            content,
-            author
-        });
+// Create a new comment
+router.post('/', (req, res) => {
+  const { author, content, blogId } = req.body;
+  const newComment = { id: comments.length + 1, author, content, blogId };
+  comments.push(newComment);
+  res.status(201).json(newComment);
+});
 
-        // Save the comment
-        const savedComment = await newComment.save();
-        res.status(201).json(savedComment);
-    } catch (err) {
-        res.status(500).json({ error: 'Unable to post the comment' });
-    }
+// Read all comments
+router.get('/', (req, res) => {
+  res.json(comments);
+});
+
+// Read a single comment
+router.get('/', (req, res) => {
+  const comment = comments.find(c => c.id == req.params.id);
+  if (!comment) return res.status(404).json({ error: 'Comment not found' });
+  res.json(comment);
+});
+
+// Update a comment
+router.put('/', (req, res) => {
+  const { author, content, blogId } = req.body;
+  const comment = comments.find(c => c.id == req.params.id);
+  if (!comment) return res.status(404).json({ error: 'Comment not found' });
+
+  comment.author = author;
+  comment.content = content;
+  comment.blogId = blogId;
+  res.json(comment);
+});
+
+// Delete a comment 
+router.delete('/', (req, res) => {
+  const commentIndex = comments.findIndex(c => c.id == req.params.id);
+  if (commentIndex === -1) return res.status(404).json({ error: 'Comment not found' });
+
+  comments.splice(commentIndex, 1);
+  res.status(204).end();
 });
 
 module.exports = router;
